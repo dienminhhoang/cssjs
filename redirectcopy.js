4b449ca741933f711ad72b74ffcc0b69e922fb21
dienminhhoang <31769117+dienminhhoang@users.noreply.github.com>
2017-09-08 20:27:01 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>HI HI DO NGOK'S MUON COPY THI VAO DAY : <a href='https://www.facebook.com/chuphinhchuyennghiephoangdien/>https://www.facebook.com/chuphinhchuyennghiephoangdien/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>HI HI DO NGOK'S MUON COPY THI VAO DAY : <a href='https://www.facebook.com/chuphinhchuyennghiephoangdien'>https://www.facebook.com/chuphinhchuyennghiephoangdien/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
